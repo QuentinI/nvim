cf4e2ec01d9698145b19fcccc1891979de27c251
Quentin Inkling <mbee@protonmail.ch>
2022-11-29 05:16:50 +0300

    add crates-nvim

--- code.nix
+++ code.nix
@@ -216,6 +216,19 @@ in
     '';
     optional = true;
   }
+  {
+    plugin = crates-nvim;
+    config = lua ''
+      require('crates').setup({})
+      vim.api.nvim_create_autocmd("BufRead", {
+          group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
+          pattern = "Cargo.toml",
+          callback = function()
+              require('cmp').setup.buffer({ sources = { { name = "crates" } } })
+          end,
+      })
+    '';
+  }
   # Autocompletion
   # https://github.com/hrsh7th/nvim-cmp
   nvim-snippy
